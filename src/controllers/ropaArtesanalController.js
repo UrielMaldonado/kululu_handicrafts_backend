b5a934b07a0b58dd20a28a87545e43c969ea5bf3
UrielMaldonado <uri.7ma.3co@gmail.com>
2023-11-08 10:18:11 -0600

    ropa-artesanal CRUD complete

--- src/controllers/ropaArtesanalController.js
+++ src/controllers/ropaArtesanalController.js
@@ -13,6 +13,54 @@ async function getAllRopaArtesanal(req, res) {
   }
 }
 
+// Obtener una pieza de ropa artesanal por ID
+async function getRopaArtesanalById(req, res) {
+  const { id } = req.params;
+  try {
+    const ropaArtesanal = await prisma.ropa_artesanal.findUnique({
+      where: { id: parseInt(id) },
+    });
+    if (ropaArtesanal) {
+      res.json(ropaArtesanal);
+    } else {
+      res.status(404).json({ error: 'La pieza de ropa artesanal no fue encontrada' });
+    }
+  } catch (error) {
+    console.error('Error al obtener la ropa artesanal por ID:', error);
+    res.status(500).json({ error: 'Ocurrió un error al obtener la pieza de ropa artesanal' });
+  }
+}
+// Eliminar una pieza de ropa artesanal por ID
+async function deleteRopaArtesanalById(req, res) {
+  const { id } = req.params;
+  try {
+    const deletedRopaArtesanal = await prisma.ropa_artesanal.delete({
+      where: { id: parseInt(id) },
+    });
+    res.json(deletedRopaArtesanal);
+  } catch (error) {
+    console.error('Error al eliminar la pieza de ropa artesanal:', error);
+    res.status(500).json({ error: 'Ocurrió un error al eliminar la pieza de ropa artesanal' });
+  }
+}
+// Actualizar una pieza de ropa artesanal por ID
+async function updateRopaArtesanalById(req, res) {
+  const { id } = req.params;
+  const updatedData = req.body; // Asegúrate de validar y sanear los datos antes de usarlos en una aplicación real.
+  try {
+    const updatedRopaArtesanal = await prisma.ropa_artesanal.update({
+      where: { id: parseInt(id) },
+      data: updatedData,
+    });
+    res.json(updatedRopaArtesanal);
+  } catch (error) {
+    console.error('Error al actualizar la pieza de ropa artesanal:', error);
+    res.status(500).json({ error: 'Ocurrió un error al actualizar la pieza de ropa artesanal' });
+  }
+}
+
 module.exports = {
   getAllRopaArtesanal,
+  getRopaArtesanalById,
+  deleteRopaArtesanalById,
 };
--- src/routes/ropaArtesanalRoute.js
+++ src/routes/ropaArtesanalRoute.js
@@ -5,5 +5,8 @@ const ropaArtesanalController = require('../controllers/ropaArtesanalController'
 
 // Ruta para obtener todas las piezas de ropa artesanal
 router.get('/ropa-artesanal', ropaArtesanalController.getAllRopaArtesanal);
+router.get('/ropa-artesanal/:id', ropaArtesanalController.getRopaArtesanalById);
+router.delete('/ropa-artesanal/:id', ropaArtesanalController.deleteRopaArtesanalById);
+router.put('/ropa-artesanal/:id', ropaArtesanalController.updateRopaArtesanalById);
 
 module.exports = router;
